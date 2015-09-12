719a5ee6e11f8cc7a52140757eff38c3c2292521
gmitrev <gvmitrev@gmail.com>
2015-09-12 18:26:37 +0300

    ch02e04 solution

---
+++ chapter-2/04.scm
@@ -0,0 +1,13 @@
+#lang scheme
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+(car (cons 1 2)) ; => 1
+(cdr (cons 1 2)) ; => 2
