286459918efa808f0859f46c7184f27223a585ab
gmitrev <gvmitrev@gmail.com>
2015-09-15 16:59:26 +0300

    c02e22 solution

---
+++ chapter-2/22.scm
@@ -0,0 +1,25 @@
+#lang scheme
+
+(define (square x) (* x x))
+
+(define (square-list items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things)
+              (cons (square (car things))
+                    answer))))
+  (iter items '()))
+
+(square-list '(1 2 3 4))
+
+(define (square-list-2 items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things)
+              (cons answer
+                    (square (car things))))))
+  (iter items '()))
+
+(square-list-2 '(1 2 3 4))
