4e3b40d182a464942133483b3b3de7c24f12a2a5
Georgi Mitrev <gvmitrev@gmail.com>
2015-12-02 22:01:01 +0200

    c02e32 solution

---
+++ chapter-2/32.scm
@@ -0,0 +1,9 @@
+#lang racket
+
+(define (subsets s)
+  (if (null? s)
+      (list null)
+      (let ((rest (subsets (cdr s))))
+        (append rest (map (λ (subset) (cons (car s) subset)) rest)))))
+
+(subsets (list 1 2 3))
