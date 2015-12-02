31e98370e891acdc198a614a512011c25de2e4ca
Georgi Mitrev <gvmitrev@gmail.com>
2015-12-02 22:30:01 +0200

    c02e34 solution

---
+++ chapter-2/34.scm
@@ -0,0 +1,15 @@
+#lang scheme
+
+(define (accumulate f initial sequence)
+  (if (null? sequence)
+      initial
+      (f (car sequence)
+         (accumulate f initial (cdr sequence)))))
+
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms) (+ (* higher-terms x) this-coeff))
+              
+              0
+              coefficient-sequence))
+
+(horner-eval 2 (list 1 3 0 5 0 1)) ; => 79
