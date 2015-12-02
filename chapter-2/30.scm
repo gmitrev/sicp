00113d0ea3f382263a71a2d9077a6cccb8ca7b1d
Georgi Mitrev <gvmitrev@gmail.com>
2015-12-02 21:52:36 +0200

    c02e30 solution

---
+++ chapter-2/30.scm
@@ -0,0 +1,24 @@
+#lang racket
+
+(define tree
+ (list 1
+       (list 2 (list 3 4) 5)
+       (list 6 7))) ; => (1 (4 (9 16) 25) (36 49))
+
+
+(define (square-tree-map tree)
+  (map (λ (sub-tree)
+         (if (list? sub-tree)
+             (square-tree-map sub-tree)
+             (* sub-tree sub-tree)))
+       tree))
+
+(square-tree-map tree)
+
+(define (square-tree-peasantly tree)
+  (cond ((null? tree) null)
+        ((not (list? tree)) (* tree tree))
+        (else (cons (square-tree-peasantly (car tree))
+                    (square-tree-peasantly (cdr tree))))))
+
+(square-tree-peasantly tree)
